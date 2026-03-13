2539516ff946a18cdd8a6ecd8eae0667528815d8
Server <nick@vmi2925439.contaboserver.net>
2026-03-14 00:52:23 +0100

    fixing settings defaults

--- apis/settings.py
+++ apis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y*%q=4)^%54_ikcoduuxo(d84iy1ce1ky+78b14=j5favl9k2g'
+SECRET_KEY = config('SECRET_KEY')
 
 # CSRF settings for nginx deployment
 CSRF_TRUSTED_ORIGINS = [
@@ -30,7 +31,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = [
     'localhost',
